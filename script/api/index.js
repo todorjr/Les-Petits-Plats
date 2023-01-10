45d9420e60eb09ce30aa3f25490bbc7eaf3be009
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-01-10 16:46:32 +0100

    recepies array

--- script/api/index.js
+++ script/api/index.js
@@ -1,6 +1,8 @@
+const recepies = [];
+
 fetch('recepies.json')
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    recepies.push(data);
+    console.log(recepies);
 });
-    // we are now have access to all data from data.json file   
