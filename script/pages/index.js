e4f0b98983ac63049e03dd9649270811d6b9827c
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-01-10 18:01:22 +0100

    api

--- script/pages/index.js
+++ script/pages/index.js
@@ -1,8 +1,8 @@
 import { getRecepies } from "../api/index.js"
- 
-function displayData(recepie) {
-    const section= document.querySelector(".container");
-    const recepies = document.querySelector("#recepies");
+
+function displayData(recepies) {
+    const section = document.querySelector(".container");
+    const recepiesSection = document.createElement("div")
 
     recepies.forEach((recepie) => {
         const title = document.createElement("h2")
@@ -10,24 +10,24 @@ function displayData(recepie) {
         title.classList.add("title-recepies")
         title.setAttribute("id", "title");
         title.innerHTML = recepie.name
-    
+
         const description = document.createElement("p")
         description.setAttribute("tabindex", 0)
         description.classList.add("description-recepies")
         description.setAttribute("id", "description");
         description.innerHTML = recepie.description
-      });
-  
+    });
 
-    recepies.appendChild(description)
-    recepies.appendChild(title)
-    section.appendChild(recepies)
-  }
+
+    recepiesSection.appendChild(description)
+    recepiesSection.appendChild(title)
+    section.appendChild(recepiesSection)
+}
 
 async function init() {
     const recepies = await getRecepies();
-    console.log(recepies,'data');
+    console.log(recepies, 'data');
     displayData(recepies);
-  }
-  
-  init();
+}
+
+init();
