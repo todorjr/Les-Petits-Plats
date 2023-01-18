e111351a5b4faa2bd4edb2333571d694be321091
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-01-18 16:41:49 +0100

    code comment

--- script/view/IngrediantList.js
+++ script/view/IngrediantList.js
@@ -1,4 +1,4 @@
-export function getIngredientListItem (ingredient) {
+export function getIngredientListItem(ingredient) {
     const item = document.createElement('a')
     const message = `${ingredient.ingredient}`
     item.innerHTML = message
@@ -8,17 +8,20 @@ export function getIngredientListItem (ingredient) {
 
 export function getIngrediants(ingredients) {
 
-const listElement = document.createElement('div')
-listElement.classList.add('dropdown-content')
+    const listElement = document.createElement('div')
+    listElement.classList.add('dropdown-content')
 
-const listItemsElement = ingredients.map(getIngredientListItem) 
-const uniqueElementList = [...new Set(listItemsElement.map(item => item.innerHTML))].map(ingredient => {
-    return listItemsElement.find(a => a.innerHTML === ingredient)
-})
+    const listItemsElement = ingredients.map(getIngredientListItem)
 
-listElement.append(...uniqueElementList)
+    // creating a new Set from the innerHTML property of each element in the listItemsElement array
+    // mapping over each item in the new Set and returning the corresponding element from the listItemsElement array that has the same innerHTML value
+    const uniqueElementList = [...new Set(listItemsElement.map(item => item.innerHTML))].map(ingredient => {
+        return listItemsElement.find(a => a.innerHTML === ingredient)
+    })
 
-return listElement
+    listElement.append(...uniqueElementList)
+
+    return listElement
 }
 
 
