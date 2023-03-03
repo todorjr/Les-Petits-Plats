d56d9f0a72aaba0340c77555b10e424b6f55e24f
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-03-03 16:31:19 +0100

    WIP

--- script/api/index.js
+++ script/api/index.js
@@ -1,15 +1,20 @@
+/** 
+ * Retrieves a JSON object containing recipe data from the specified URL. Returns a Promise that resolves with the recipe data, or rejects with an error if the network response is not ok.
+@returns {Promise} A Promise that resolves with the recipe data, or rejects with an error if the network response is not ok.
+*/
+
 export async function getRecipes() {
     return fetch('data/recepies.json')
         .then((response) => {
-        if (response.ok) {
-            return response.json();
-        } else {
-            throw new Error("NETWORK RESPONSE ERROR");
-        }
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("NETWORK RESPONSE ERROR");
+            }
         })
         .then(data => {
             return data;
-          })
+        })
         .catch((error) => console.error("FETCH ERROR:", error));
 }
 
--- script/view/SearchRecipes.js
+++ script/view/SearchRecipes.js
@@ -1,5 +1,12 @@
 import { renderRecipes } from "../pages/index.js";
 
+/**
+ * Searches through the given array of recipe objects for matching recipes based on the given user input. Each recipe object must have a searchText property  which contains a string of keywords that can be used for searching. Returns an array of recipe objects that match the given user input.
+@param {string} userInput The user input to search for.
+@param {Array} recipes An array of recipe objects to search through.
+@returns {Array} An array of recipe objects that match the given user input.
+*/
+
 export function searchAllRecipes(userInput, recipes) {
     const filteredRecipes = recipes.filter(recipe => {
         return recipe.searchText && recipe.searchText.includes(userInput);
@@ -9,6 +16,10 @@ export function searchAllRecipes(userInput, recipes) {
     });
 }
 
+/** 
+ * createSearchInputElement() creates a search input element and attaches an event listener that listens to input changes. If the input value is 3 or more characters long, it searches through the given data for matching recipes and renders them. If the input value is less than 3 characters long, it renders all the recipes from the given data. If no matching recipes are found, it renders a message indicating that no results were found.
+@param {Array} data An array of recipe objects to search through and render.
+*/
 export function createSearchInputElement(data) {
     const userInput = document.querySelector("#userInput");
     const resultsContainer = document.querySelector(".container");
