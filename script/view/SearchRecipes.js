11cbd1e173a9315ce49021468c84817b7ec66962
Lazar V. Todorovic <lazar.todorovic@jack.agency>
2023-03-03 15:43:17 +0100

    WIP: search items in dropdowns

--- script/view/SearchRecipes.js
+++ script/view/SearchRecipes.js
@@ -1,15 +1,15 @@
-import { displayData } from "../pages/index.js";
+import { renderRecipes } from "../pages/index.js";
 
 export function searchAllRecipes(userInput, recipes) {
     const filteredRecipes = recipes.filter(recipe => {
-      return recipe.searchText && recipe.searchText.includes(userInput);
+        return recipe.searchText && recipe.searchText.includes(userInput);
     });
     return filteredRecipes.map(recipe => {
-      return recipe.recipe;
+        return recipe.recipe;
     });
-  }
+}
 
-  export function acceptInput(data) {
+export function createSearchInputElement(data) {
     const userInput = document.querySelector("#userInput");
     const resultsContainer = document.querySelector(".container");
 
@@ -18,34 +18,13 @@ export function searchAllRecipes(userInput, recipes) {
             const recipes = searchAllRecipes(userInput.value, data);
             if (recipes.length === 0) {
                 resultsContainer.innerHTML = `<p class="no-results">No results found for "${userInput.value}" ! 🚫</p>`;
-            }else{
-                displayData(recipes);
-            } 
-         }else{
-            if( userInput.value.length < 1) {
-                displayData(data.map(recipe => recipe.recipe))
+            } else {
+                renderRecipes(recipes);
             }
-         }
-    });
-}
-
-
-export function searchOptions(data, type) {
-    const userInput = document.querySelector(`.list-${type}-item`);
-    const resultsContainer = document.querySelector(".container");
-    userInput.addEventListener("click", () => {
-        let recipes;
-        if (type === "ingrediant") {
-            recipes = searchRecipesByIngredient(userInput, data.recipes);
-        } else if (type === "appliance") {
-            recipes = searchRecipesByAppliance(userInput, data.recipes);
-        } else if (type === "ustensil") {
-            recipes = searchRecipesByUstensil(userInput, data.recipes);
-        }
-        if (recipes.length === 0) {
-            resultsContainer.innerHTML = `<p class="no-results">No results found for "${userInput.value}" ! 🚫</p>`;
         } else {
-            displayData(recipes);
+            if (userInput.value.length < 1) {
+                renderRecipes(data.map(recipe => recipe.recipe))
+            }
         }
     });
 }
